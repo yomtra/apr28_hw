7706cb5bebb5ec5d23832bb2a4213205b2e48912
yomtra <bugarinartyom2@gmail.com>
2025-04-28 16:59:44 -0500

    up to rule loop

--- main.tf
+++ main.tf
@@ -4,4 +4,16 @@ resource "aws_security_group" "sg_groups"{
     name = each.value.name
     description = each.value.description
     vpc_id = each.value.vpc_id
+
+    tags = {
+        Name = each.value.name
+    }
 }
+
+
+# resource "aws_security_group_rule" "ingress" {
+#     for_each = {
+        
+#     }
+# }
+
--- variables.tf
+++ variables.tf
@@ -1,8 +1,22 @@
-#creates a variable that lets me make multiple security groups dynamically
+#make multiple security groups dynamically
 variable "security_groups" {
   type = map(object({
     name        = string
     description = string
     vpc_id      = string
+    ingress     = list(object({
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks  = list(string)
+      description = string
+    }))
+    egress      = list(object({
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks  = list(string)
+      description = string
+    }))
   }))  
 }
