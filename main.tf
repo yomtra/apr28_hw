1ca0265d05e66c4e1f91ae3b688f5a7b44210051
yomtra <bugarinartyom2@gmail.com>
2025-04-28 16:18:54 -0500

    sec group

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
-resource "aws_security_group" "security_group"{
-    for_each = 
+resource "aws_security_group" "sg_groups"{
+    for_each = var.security_groups
+    
+    name = each.value.name
+    description = each.value.description
+    vpc_id = each.value.vpc_id
 }
